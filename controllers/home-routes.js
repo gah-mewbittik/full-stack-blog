7f3a09d395b137220363086fcddae20c5aafad4e
Andrew Biron <andrewbiron@andrews-air.home>
2024-02-25 17:34:02 -0500

    split routes into their own files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,44 +1,48 @@
 const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const { User, Post, Comment } = require('../models');
+//const withAuth = require('../utils/auth');
 
 // Get homepage
 router.get('/', (req, res) => {
   res.render('homepage');
 });
 
-// Prevent non logged in users from viewing the Dashboard
-router.get('/dashboard', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+// // Prevent non logged in users from viewing the Dashboard
+// router.get('/dashboard', withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       order: [['name', 'ASC']],
+//     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+//     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('homepage', {
+//       users,
+//       // Pass the logged in flag to the template
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 
 // Get Login
-router.get('/login', (req, res) => {
-  // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+// router.get('/login', (req, res) => {
+//   // If a session exists, redirect the request to the homepage
+//   if (req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
 
-  res.render('login');
-});
+//   res.render('login');
+// });
 
 // get singUp
+
+
+
 router.get('/signUp', (req, res) => {
   res.render('signUp');
 });
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashRoutes = require('./dash-routes');
+const loginRoutes = require('./login-routes');
 
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
+router.use('/login', loginRoutes);
 
 router.use('/api', apiRoutes);
 
