1d6de7fcb2271eeeed52ec76a7137afb2ca91cfe
Andrew Biron <andrewbiron@andrews-air.home>
2024-03-19 11:55:46 -0400

    added bootstrap alert for successful sign up

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -6,6 +6,8 @@ const signupFormHandler = async (event) => {
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
+    let container = document.querySelector('#alert-container');
+
     if (name && email && password) {
       const response = await fetch('/api/users/signup', {
         method: 'POST',
@@ -15,7 +17,29 @@ const signupFormHandler = async (event) => {
   
       if (response.ok) {
         document.location.replace('/dashboard');
+
+        container.innerHTML = 
+        `<div class="alert alert-success alert-dismissible fade show" role="alert">
+        Signed Up! You can now login!
+            <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+                
+            </button>
+        </div>`;
+        // $(container).on('closed.bs.alert', function () {
+        //     console.log('Alert is closed');
+        // })
+        const closeBtn = container.querySelector('.btn-close');
+
+        closeBtn.addEventListener('click', function(){
+            const alert = this.closest('.alert');
+            if(alert){
+                alert.style.display = 'none';
+                console.log('Alert is closed');
+            }
+        });
+
       } else {
+        console.log('Response Error: ', response.statusText);
         alert(response.statusText);
       }
     }
